15e3ae57e2de931e3ce1649bd0f142e965a77fcf
MBIlalsheikh05 <mbilalsheikh05@gmail.com>
2024-08-16 16:27:54 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Navigate, Route, Routes} from 'react-router-dom'
+import AuthForm from './AuthForm';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Navigate to='/login'/>}/>
+        <Route path='/authform' element={<AuthForm/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/home' element={<Home/>} />
+      </Routes>
     </div>
+    
+  
   );
 }
 
 export default App;
+
+
